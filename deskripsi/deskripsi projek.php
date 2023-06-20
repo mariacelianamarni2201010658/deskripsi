ed1faa633c98eece694a799ea19f90058e025e2e
mariacelianamarni2201010658 <127914115+mariacelianamarni2201010658@users.noreply.github.com>
2023-06-20 19:10:44 +0700

    Add files via upload

---
+++ deskripsi/deskripsi projek.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>DESKRIPSI ROJEK</h1>
+    <h3>A nama projek:ayana hotel</h3>
+    <h3>B deskripsi projek: projek ini bertuujuan untuk menampilkan web tentang AYANA hotel beserta fasilitas-fasilitasnya
+        gambar utama
+        gambar 1:tentang keseluruhan AYANA hotel labuan bajo
+        gambar 2:view dari lantai 13 diayana hotel dipagi hari
+        gambar 3:kamar
+        gambar 4:restaurant
+        gambar 5:suasana dari lobi
+        gambar 6:kapal
+        gambar 7:kolam
+        gambar 8:meting room
+        gambar 9:gym 
+        maps
+
+    </h3>
+    <h3> C penjelasan database</h3>
+        <h4>1.firs name:kolom ini akan menyimpan nama pertama pengguna</h4>
+        <h4>2.last name: kolom ini akan menyimpan nama terakhir dari pengguna</h4>
+        <h4>3.email:kolom ini akan menyimpan email dari pengguna</h4>
+        <h4>4.password:kolom ini akan menyimpan password email dari pengguna</h4>
+        
+    <h3> D bahasa pemograman yang digunakan</h3>
+    <h4>
+        1 HTML
+        digunakan menampilkan database, dimana untuk melakukan oprasi pemorsesan dalam melihat halam utama web 
+        atau tampilan tentang AYANA hotel
+    </h4>
+    <h4>
+        2 PHP
+        digunakan untuk menampilkan tampilan dasar halaman web tentang AYANA hotel beserta vasilitas-vasilitasnya
+    </h4>
+
+</body>
+</html>
---
+++ deskripsi/ide projek.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h1> IDE PROJEK </h1>
+        <h4>
+            ide projek dari website ini adalah:menciptakan sebuh web dengan menampilkan tentang AYANA hotel
+        </h4>
+        <h4>
+           tujuan dari dibuatnya projek ini adalah:untuk menyediakan website yang mudah digunakan untuk 
+           melihat tentang AYANA hotel,beserta fasilitas-fasilitas didalamnya
+        </h4>
+</body>
+</html>
